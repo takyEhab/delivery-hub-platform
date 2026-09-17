a62812d28d71836c5b20f7fecb7bea679ae755e2
takyEhab <tkyehab@gmail.com>
2026-09-17 18:56:42 +0300

    restricting only my frontend app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { FRONTEND_URL } = require('./config/env');
 const { notFoundHandler, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: FRONTEND_URL || "https://delivery-hub-platform.vercel.app",
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(routes);
 app.use(notFoundHandler);
